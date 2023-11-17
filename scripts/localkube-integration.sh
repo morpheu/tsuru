f41c1cff1bbaac88d196d87e0570705e685580ac
Paulo Sousa <paulo.sousa@corp.globo.com>
2023-11-17 10:19:39 -0300

    add debug to helm

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -42,7 +42,7 @@ install_tsuru_stack() {
     --set tsuru-api.image.repository=localhost/tsuru/tsuru-api \
     --set tsuru-api.image.tag=integration \
     --set tsuru-api.image.pullPolicy=Never \
-    --timeout 10m --wait \
+    --timeout 5m --wait --debug \
     tsuru tsuru/tsuru-stack
 }
 
